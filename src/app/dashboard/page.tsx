e3a85454d20710bed078f8edb07eed03184a4514
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-11-02 07:41:46 -0600

    add dashboard page

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function DashboardPage() {
+  return (
+    <main className="w-full min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Welcome to dashboard</h1>
+    </main>
+  )
+}
