589eb14b77526308aaee7dbc8eb22cac8964c2f1
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-11-02 08:05:27 -0600

    feat: add AppRouterContextProviderMock for testing and update home test to use it

--- __tests__/home.test.tsx
+++ __tests__/home.test.tsx
@@ -1,4 +1,6 @@
-import Home from "@/app/page";
+import HomePage from "@/app/page";
+import { QueryProviderWrapper } from "@/app/QueryProviderWrapper";
+import { AppRouterContextProviderMock } from "@/shared/ui/organisms/AppRouterContextProviderMock";
 import { render, screen } from "@testing-library/react";
 
 // Mock ResizeObserver
@@ -8,8 +10,35 @@ global.ResizeObserver = jest.fn().mockImplementation(() => ({
   disconnect: jest.fn(),
 }));
 
-it('renders the home page with welcome message', () => {
-  render(<Home />);
+const Home = async ({ push }: { push: () => void }) => {
+  const Page = await HomePage()
+  return (
+    <QueryProviderWrapper>
+      <AppRouterContextProviderMock router={{ push }}>
+        {Page}
+      </AppRouterContextProviderMock>
+    </QueryProviderWrapper>
+  )
+}
 
-  expect(screen.getByRole('heading', { name: /welcome to my app/i })).toBeInTheDocument();
+describe('Login', () => {
+  beforeEach(() => {
+    jest.spyOn(console, 'error').mockImplementation(() => {});
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+    jest.restoreAllMocks();
+  })
+
+  it('Show the login page', async () => {
+    const push = jest.fn();
+    render(await Home({ push }))
+ 
+    expect(screen.getByRole('heading', { name: /bienvenido de vuelta/i })).toBeInTheDocument()
+    expect(screen.getByText(/ingrese sus credenciales para entrar a su cuenta\./i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/correo electrónico/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/contraseña/i)).toBeInTheDocument()
+    expect(screen.getByRole('button', { name: /iniciar sesión/i })).toBeInTheDocument()
+  })
 })
