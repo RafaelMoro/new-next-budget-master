afbae5d537e4eedf8ee3e175ae68378d52d7d8d9
Rafael Moro <rafael.moro.galindo@gmail.com>
2025-11-04 09:41:14 -0600

    Add OlderRecordsAccordion and DateRangeSearchPanel components; implement SelectMonthDropdown and SelectYearDropdown for month and year selection

---
+++ src/shared/ui/atoms/SelectYearDropdown.tsx
@@ -0,0 +1,34 @@
+import { Button, Dropdown, DropdownItem } from "flowbite-react"
+import { RiArrowDownSLine } from "@remixicon/react"
+
+import { getDateInfo } from "@/shared/utils/getDateInfo";
+
+interface SelectYearDropdownProps {
+  selectedYear: string | null
+  changeSelectedYear: (newYear: string) => void
+}
+
+/**
+* Component to select a year from a dropdown.
+* It's meant to be used in the useSelectYear hook.
+*/
+export const SelectYearDropdown = ({ selectedYear, changeSelectedYear: changeSelectedYear }: SelectYearDropdownProps ) => {
+  const { years } = getDateInfo()
+
+  return (
+    <Dropdown label="" renderTrigger={() => (
+      <Button data-testid="select-year-dropdown-button" color="light">
+        { selectedYear ? selectedYear : 'Año:' }
+        <RiArrowDownSLine />
+      </Button>
+    )}>
+      { years.map((year, index) => (
+        <DropdownItem
+          onClick={() => changeSelectedYear(year)}
+          value={year}
+          key={`${year}-${index}`}
+        >{year}</DropdownItem>
+      ))}
+    </Dropdown>
+  )
+}
